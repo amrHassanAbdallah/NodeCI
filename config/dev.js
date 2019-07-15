b7559f7c75bbf2f269d641e19951b0e8c23bdaf9
Amr Hassan <amrh434@gmail.com>
2019-07-15 20:25:43 +0200

    feat : uploading image, Added image input to the blog creation form.

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
       "195372554782-8jj3nk3c651hps9hl8bm45b7mrg2pn82.apps.googleusercontent.com",
-    googleClientSecret: "6vS88tXuI0_H-GVvCMP-4f3v",
+  googleClientSecret: "XpD5nk4Uxw7dTx2dirk62tsO",
     mongoURI: "mongodb://amrh:123456789a@ds161316.mlab.com:61316/learning-node",
     cookieKey: "123123123"
 };
