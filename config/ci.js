acd5bdc93fa28b3004de6ed25a09a1c268f631f9
Amr Hassan <amrh434@gmail.com>
2019-07-15 19:03:36 +0200

    chore : travis CI config.

--- config/ci.js
+++ config/ci.js
@@ -1,7 +1,7 @@
 module.exports = {
     googleClientID:
-        "",
-    googleClientSecret: "",
+        "195372554782-8jj3nk3c651hps9hl8bm45b7mrg2pn82.apps.googleusercontent.com",
+    googleClientSecret: "6vS88tXuI0_H-GVvCMP-4f3v",
     mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
     cookieKey: "123123123",
     redisUrl: "redis://127.0.0.1:6379"
